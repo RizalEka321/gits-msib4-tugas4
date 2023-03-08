1cc674c8bd985d3689df56a636870ad924e8b06a
RizalEka321 <hahaharizal6@gmail.com>
2023-03-08 20:08:40 +0700

    feat:tambah static function model mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    static function jumlahMahasiswa()
+    {
+        return DB::select('select count(*) as total from mahasiswas');
+    }
 }
