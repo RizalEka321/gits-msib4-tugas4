4bfa426b0810d31d8cc00dd2cb461b1f480ad724
RizalEka321 <hahaharizal6@gmail.com>
2023-03-08 20:06:26 +0700

    feat:edit migration mahasiswa

--- database/migrations/2023_03_08_130044_create_mahasiswas_table.php
+++ database/migrations/2023_03_08_130044_create_mahasiswas_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->enum('prodi',['Teknologi Rekayasa Perangkat Lunak','Teknik Rekayasa Komuputer','Bisnis Digital']);
+            $table->enum('status', ['Aktif', 'Tidak Aktif']);
             $table->timestamps();
         });
     }
