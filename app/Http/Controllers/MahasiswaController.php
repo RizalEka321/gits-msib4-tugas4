a66c02f01f1aab2d483929eb69495946e0b32881
RizalEka321 <hahaharizal6@gmail.com>
2023-03-08 20:12:00 +0700

    feat:tambah function index di MahasiswaController

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    //
+    function index()
+    {
+        $mentor = "Rizaldi Permana";
+        $program = "Studi Independent";
+        $mitra = "PT GITS Indonesia";
+        $jumlah = Mahasiswa::jumlahMahasiswa();
+        $mahasiswa = Mahasiswa::paginate(10);
+        return view('mahasiswa', [
+            'mentor' => $mentor,
+            'program' => $program,
+            'mitra' => $mitra,
+            'jumlah' => $jumlah,
+            'mahasiswa' => $mahasiswa
+        ]);
+    }
 }
